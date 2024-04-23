9420a15f46e9935700b86fff32c6b8596f62f280
Thomas-Deblay <thomasdeblay.dev@gmail.com>
2024-04-23 16:18:06 +0200

    final result with that need to be tested with GTM and GA4 debug view

--- js/atsTrackingAfterPageLoad.js
+++ js/atsTrackingAfterPageLoad.js
@@ -719,7 +719,10 @@ $(function(){
 			/* tracking of Ecommerce refund action begin
 			* use cancelOrder.orderRef JS variable to get the canceled order Id
 			*/
-
+			gtag("event", "refund", {
+				currency: "USD",
+				transaction_id: cancelOrder.orderRef, // Transaction ID. Required for purchases and refunds.
+			  });
 			// tracking of Ecommerce refund action end
 		});
 	}
