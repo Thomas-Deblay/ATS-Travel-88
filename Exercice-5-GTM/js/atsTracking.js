b3eb1311bc7885010079a5bf5dffe13a5fec8515
Thomas-Deblay <thomasdeblay.dev@gmail.com>
2024-04-25 11:12:35 +0200

    re-corrected GTM config

--- Exercice-5-GTM/js/atsTracking.js
+++ Exercice-5-GTM/js/atsTracking.js
@@ -58,8 +58,8 @@ console.log("EXERCICES w/ GTM")
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id=GTM-5LP5C32Z'+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5LP5C32Z')
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5LP5C32Z');
 
 // tracking of Page end
 
