45d311abd4309c53659c24bc1930bd91ce81feef
Thomas-Deblay <thomasdeblay.dev@gmail.com>
2024-04-26 16:25:50 +0200

    Update atsTrackingAfterPageLoad.js

--- Exercice-5-GTM/js/atsTrackingAfterPageLoad.js
+++ Exercice-5-GTM/js/atsTrackingAfterPageLoad.js
@@ -116,7 +116,7 @@ $(function(){
 		$("#viewDestination a").on('click',function(e){
 			/* tracking of View Destinations button click begin
 			*/
-			
+			window.dataLayer.push({'event': 'cta_click', 'click_category':'home', 'click_action': 'click','click_label': 'view destinations' });
 			// tracking of View Destinations button click end
 		});
 		
