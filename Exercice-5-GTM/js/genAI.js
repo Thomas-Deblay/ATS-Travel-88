892b79f8c93b179f41a1bb18676d0290168d5dca
Thomas-Deblay <thomasdeblay.dev@gmail.com>
2024-11-26 04:53:16 +0100

    trying to figure out what goes wrong

--- Exercice-5-GTM/js/genAI.js
+++ Exercice-5-GTM/js/genAI.js
@@ -203,7 +203,7 @@ function checkIfDLVVariable(variableName){
 function returnListOfVaribaleNamesFromList(list){
     const listOfNames = list.map((x) => x.map[1].value);
 
-    return listOfNames.filter((x) => checkIfDLVVariable(x));
+    return listOfNames;
 }
 
 function getEventSettingsVariables(){
