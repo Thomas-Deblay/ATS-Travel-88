3599e0cbb1705f48a71bb7226f343a7b2e3e2abf
Thomas-Deblay <thomasdeblay.dev@gmail.com>
2024-12-05 15:01:14 +0100

    deleted effort to add page view GA4 event settings

--- Exercice-5-GTM/js/genAI.js
+++ Exercice-5-GTM/js/genAI.js
@@ -278,7 +278,7 @@ function getEventSettingsVariables(){
     return returnListOfVaribaleNamesFromList(listOfVaribales);
 }
 
-function createDataLayerPush(event, variables, sendEcommerceData, send, GA4Event){
+function createDataLayerPush(event, variables, sendEcommerceData, send){
     let push = 'dataLayer.push(\n{\n';
     if(event){
         push += `event : ${event},\n`;
@@ -292,13 +292,6 @@ function createDataLayerPush(event, variables, sendEcommerceData, send, GA4Event
 
     if(send === "true"){
         push += sendEcommerceData.push;
-    }
-
-    if(GA4Event.includes("page_view")){
-        for(let variable of eventSettingsVariablesArray){
-            push += `${variable} : $${variable},\n`;
-        }
-       
     }
 
     push += '}\n)';
@@ -458,7 +451,7 @@ function createTable(){
     tableauEventPush.forEach((x) => {
         const event = x.GA4Events.length > 0 ? x.GA4Events.toString() : "none";
         const trigger = x.triggerEvent.name;
-        const push = createDataLayerPush(x.triggerEvent.name, x.pushEvent.push, x.pushEvent.sendEcommerceData ,x.pushEvent.sendEcommerceData.send, event );
+        const push = createDataLayerPush(x.triggerEvent.name, x.pushEvent.push, x.pushEvent.sendEcommerceData ,x.pushEvent.sendEcommerceData.send );
     
         const colonne = new specTable(event, trigger, push);
     
